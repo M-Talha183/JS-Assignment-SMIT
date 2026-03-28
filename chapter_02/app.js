9d13e64cdd76172b7ece7e611b3631e08fd02ae3
M-Talha183 <shahzainali5002@gmail.com>
2026-03-28 12:40:58 +0500

    Chapter 02 complete

---
+++ chapter_02/app.js
@@ -0,0 +1,38 @@
+// chapter 02
+
+// answer 01
+let username;
+
+// answer 02
+let myName = "Muhammad Talha"
+
+// answer 03
+let message = "Hello world"
+// alert(message)
+
+// answer 04
+let name = "Muhammad Talha"
+let age = 21
+let qulification = "Modren Web Developer "
+// alert(name)
+// alert(age)
+// alert(qulification)
+
+// answer 05
+let pattern = "pizza\npizz\npiz\npi\np";
+alert(pattern);
+
+// answer 06
+let email = "abc@gmail.com"
+alert("My Email Address is "+" "+email)
+
+// answer 07
+let book = "A smater way to learn Javascript"
+alert("I am Trying to learn from the book  "+" "+book)
+
+// answer 08
+document.write("Yeh I can write HTML content THrough Javascript ");
+
+// answer 09
+let emoji = "-------------- some content show here given by User --------------------"
+alert(emoji)
