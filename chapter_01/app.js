5f85051aa00d551d6074a6ef23d017141218e4d8
M-Talha183 <shahzainali5002@gmail.com>
2026-03-28 12:29:58 +0500

    Chapter 01 complete

---
+++ chapter_01/app.js
@@ -0,0 +1,23 @@
+// chapter 1
+// answer 1
+alert("Hell saaaad ")
+
+// answer 2
+alert("Error ! please Enter the valid password ")
+
+// answer 3
+alert("Welcomne to JS land ... \nHappy Codding")
+
+// answer 4
+
+// alert("Welcome to JS land ")
+alert ("Happy coding ")
+
+// answer 5
+alert("Hello! This is an alert from the console.")
+
+// answer 6 
+alert("Done ")
+
+// answer 6
+alert("Done THe Task check the HTML File ")
