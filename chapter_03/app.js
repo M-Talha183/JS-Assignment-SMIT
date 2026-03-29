5940d601f21e2fb3d937f681d8b9c879b6ed9618
M-Talha183 <shahzainali5002@gmail.com>
2026-03-29 07:04:25 +0500

    Complete chapter 03

---
+++ chapter_03/app.js
@@ -0,0 +1,24 @@
+// chapter 03
+
+// answer 1
+let myAge = 21;
+alert("I am " + myAge + " years old");
+
+// answer 2
+// Declare & initialize variable
+var visitCount = 5;
+
+    // Show on webpage
+document.writeln("You have visited this site " + visitCount + " times");
+
+// answer 3
+let birthYear = 2004
+document.writeln("MY Birth Year is",birthYear,"\nThe variable birth year is declare in Number Data Type ")
+
+// answer 4
+let visitName = prompt("Enter the Visitor Name ");
+let productTitle = prompt("Enter the product Title ");
+let quantity = +prompt("Enter the quantity ");
+document.writeln(visitName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store")
+
+// answer 5
