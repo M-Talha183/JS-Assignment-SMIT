1d64203cb73f4769ce1caf4bbd78872367be3831
M-Talha183 <shahzainali5002@gmail.com>
2026-04-15 23:11:24 +0500

    add chapter 17 to 20 tasks for loops

--- chapter_17-20/app.js
+++ chapter_17-20/app.js
@@ -15,5 +15,30 @@ let matrix = [
 
 // Write a program to print numeric counting from 1 to 10.
 for (let i = 1; i <= 10; i++) {
-  console.log(i \n);
+  console.log(i,'\n');
+}
+
+// Write a program to print multiplication table of any
+// number using for loop. Table number & length should be
+// taken as an input from user
+
+// user_num = +prompt("Enter the table number ")
+
+// user_count = +prompt("Enter the user count ")
+
+// for (i= 1 ; i<=user_count ; i++){
+//     document.writeln(`${user_count} * ${i} = ${user_count * i} <br>`)
+// }
+
+// Write a program to print items of the following array
+// using for loop:
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+// “strawberry”]
+
+let fruits = ["apple", "banana", "mango", "orange", "strawberr"]
+
+for (let i = 0; i<fruits.length ; i++){
+    document.writeln(`
+        Element At Index 0 is ${fruits[i] } <br>
+        `)
 }
