60185494db0fc0e1bd9fbe330d6068abe4a96729
M-Talha183 <ullahehsan016@gmail.com>
2026-06-20 16:26:30 +0500

    Add local storage questions

--- chapter_38-42/app.js
+++ chapter_38-42/app.js
@@ -1,85 +1,116 @@
-// Write a custom function power ( a, b ), to calculate the value of
-// a raised to b.
+// // Write a custom function power ( a, b ), to calculate the value of
+// // a raised to b.
 
-// let user_num_1 = +prompt("Enter the base number ....... ")
-// let user_num_2 = +prompt("Enter the power number ........")
+// // let user_num_1 = +prompt("Enter the base number ....... ")
+// // let user_num_2 = +prompt("Enter the power number ........")
 
-// function power_checker(a,b){
-//     document.write(`<p>${a} raised to ${b} is = ${Math.pow(a,b)} </p> `)
-// }
+// // function power_checker(a,b){
+// //     document.write(`<p>${a} raised to ${b} is = ${Math.pow(a,b)} </p> `)
+// // }
 
-// power_checker(user_num_1,user_num_2)
+// // power_checker(user_num_1,user_num_2)
 
-// Write a custom function power ( a, b ), to calculate the value of
-// a raised to b.
+// // Write a custom function power ( a, b ), to calculate the value of
+// // a raised to b.
 
-// let user_input = +prompt("Enter the Year ....... ")
-// console.log(typeof(user_input))
+// // let user_input = +prompt("Enter the Year ....... ")
+// // console.log(typeof(user_input))
 
-// function leap_year_checjer(year){
-//     if (year %  100 != 0 & year % 4 == 0){
-//         document.write(`<p>${year} is leap year </p> `)
-//     }else{
-//      document.write(`<p>${year} is not  leap year </p> `)
+// // function leap_year_checjer(year){
+// //     if (year %  100 != 0 & year % 4 == 0){
+// //         document.write(`<p>${year} is leap year </p> `)
+// //     }else{
+// //      document.write(`<p>${year} is not  leap year </p> `)
    
-//     }
+// //     }
+// // }
+// // leap_year_checjer(user_input)
+
+
+
+// // If the lengths of the sides of a triangle are denoted by a, b, and
+// // c, then area of triangle is given by
+// // area = S(S − a)(S − b)(S − c)
+// // where, S = ( a + b + c ) / 2
+// // Calculate area of triangle using 2 functions
+
+// // let lenght_a = +prompt("Enter the length of side a ....... ")
+// // let lenght_b = +prompt("Enter the length of side b ....... ")
+// // let lenght_c = +prompt("Enter the length of side c ....... ")
+
+// // function area_of_triangle(a,b,c){
+// //     let s = (a + b + c ) / 2
+// //     area = s * (s-a) * (s-b) * (s-c)
+
+// //     document.write(`Area of Triangle is ${area}`)
+
+// // }
+
+// // area_of_triangle(lenght_a,lenght_b, lenght_c)
+
+
+// // Write a function that receives marks received by a student in 3
+// // subjects and returns the average and percentage of these
+// // marks. there should be 3 functions one is the mainFunction
+// // and other are for average and percentage. Call those functions
+// // from mainFunction and display result in mainFunction.
+
+
+// let marks_1 = +prompt("Enter the marks of subject 1 ....... ")
+// let marks_2 = +prompt("Enter the marks of subject 2 ....... ")
+// let marks_3 = +prompt("Enter the marks of subject 3 ....... ")
+
+
+// function mainFunction (){
+//     let average_marks = average(marks_1, marks_2, marks_3)
+//     let percentage_marks = percentage(marks_1, marks_2, marks_3)
+//     document.write(`<p>Average Marks is ${average_marks} </p> `)
+//     document.write(`<p>Percentage Marks is ${percentage_marks} % </p> `)
+
+
 // }
-// leap_year_checjer(user_input)
 
-
-
-// If the lengths of the sides of a triangle are denoted by a, b, and
-// c, then area of triangle is given by
-// area = S(S − a)(S − b)(S − c)
-// where, S = ( a + b + c ) / 2
-// Calculate area of triangle using 2 functions
-
-// let lenght_a = +prompt("Enter the length of side a ....... ")
-// let lenght_b = +prompt("Enter the length of side b ....... ")
-// let lenght_c = +prompt("Enter the length of side c ....... ")
-
-// function area_of_triangle(a,b,c){
-//     let s = (a + b + c ) / 2
-//     area = s * (s-a) * (s-b) * (s-c)
-
-//     document.write(`Area of Triangle is ${area}`)
-
+// function average(marks_1, marks_2, marks_3){
+//     let average_marks = (marks_1 + marks_2 + marks_3) / 3
+//     return average_marks
 // }
 
-// area_of_triangle(lenght_a,lenght_b, lenght_c)
+
+// // question 4
+// function percentage(marks_1, marks_2, marks_3){
+//     let total_marks = 300
+//     let obtained_marks = marks_1 + marks_2 + marks_3
+//     let percentage_marks = (obtained_marks / total_marks) * 100
+//     return percentage_marks
+// }
 
 
-// Write a function that receives marks received by a student in 3
-// subjects and returns the average and percentage of these
-// marks. there should be 3 functions one is the mainFunction
-// and other are for average and percentage. Call those functions
-// from mainFunction and display result in mainFunction.
+
+// Store your name in Local Storage and display it in console.
+
+// let user_input = prompt("Enter your name ........")
+
+// localStorage.setItem("name",user_input)
+
+// Store your age and retrieve it.
+// user_age = prompt("Enter your Age ...")
+
+// localStorage.setItem("age",user_age)
+
+// let get_age = localStorage.getItem('age')
+
+// document.write(`<p>${get_age}</p>`)
 
 
-let marks_1 = +prompt("Enter the marks of subject 1 ....... ")
-let marks_2 = +prompt("Enter the marks of subject 2 ....... ")
-let marks_3 = +prompt("Enter the marks of subject 3 ....... ")
+// ["HTML","CSS","JS"]
 
+let arr = ["HTML","CSS","JS"]
 
-function mainFunction (){
-    let average_marks = average(marks_1, marks_2, marks_3)
-    let percentage_marks = percentage(marks_1, marks_2, marks_3)
-    document.write(`<p>Average Marks is ${average_marks} </p> `)
-    document.write(`<p>Percentage Marks is ${percentage_marks} % </p> `)
+localStorage.setItem("course",arr)
 
-
-}
-
-function average(marks_1, marks_2, marks_3){
-    let average_marks = (marks_1 + marks_2 + marks_3) / 3
-    return average_marks
-}
-
-
-// question 4
-function percentage(marks_1, marks_2, marks_3){
-    let total_marks = 300
-    let obtained_marks = marks_1 + marks_2 + marks_3
-    let percentage_marks = (obtained_marks / total_marks) * 100
-    return percentage_marks
-}
+let getData = localStorage.getItem("course")
+document.write(`<p>${typeof(getData) }</p> <br> <p>${getData.slice(0,4)}</p>
+<br>
+<p>${getData.slice(5,8)}</p>
+<br>
+<p>${getData.slice(9)}</p>`)
